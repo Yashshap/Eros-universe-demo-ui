079b29dc09ff3538b9ab4d309f3d6d4bbb104f8c
yash <yash.shapane@Logituit.com>
2025-06-13 12:38:21 +0530

    Initial Commit

---
+++ Eros universe demo ui/Eros_universe_demo_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eros_universe_demo_uiApp.swift
+//  Eros universe demo ui
+//
+//  Created by Apple on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Eros_universe_demo_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
